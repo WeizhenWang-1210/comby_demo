1eb3f893f50133749b1466a1c9ce627f35d97be2
weizhen <wwz011210@g.ucla.edu>
2024-11-20 11:40:44 -0800

    Boiler plate for demo. Have numerous errors.

--- tests/tests.py
+++ tests/tests.py
@@ -32,7 +32,7 @@ class MyTestCase(unittest.TestCase):
     def test_divide(self):
         c1 = ComplexNumber(1, 2)
         c2 = ComplexNumber(2, 3)
-        div = c1 * c2
+        div = c1 / c2
         self.assertEqual(div, ComplexNumber(8/13, 1/13))
         
     def test_conjugate(self):
