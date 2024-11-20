e6883f29b9f6b3673f9a228dfe9c85cbcafc674b
weizhen <wwz011210@g.ucla.edu>
2024-11-20 11:45:48 -0800

    Boiler plate for demo. Have numerous errors.

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,7 @@
 import unittest
 from lib.ComplexNumber import ComplexNumber
+
+
 class MyTestCase(unittest.TestCase):
     def test_initialization(self):
         c1 = ComplexNumber(1, 2)
@@ -15,33 +17,30 @@ class MyTestCase(unittest.TestCase):
         c1 = ComplexNumber(1, 2)
         c2 = ComplexNumber(2, 3)
         sum = c1 + c2
-        self.assertEqual(sum, ComplexNumber(3,5))
+        self.assertEqual(sum, ComplexNumber(3, 5))
 
-    def test_substract(self):
+    def test_subtract(self):
         c1 = ComplexNumber(1, 2)
         c2 = ComplexNumber(2, 3)
         diff = c1 - c2
-        self.assertEqual(diff, ComplexNumber(-1,-1))
-        
+        self.assertEqual(diff, ComplexNumber(-1, -1))
+
     def test_multiply(self):
         c1 = ComplexNumber(1, 2)
         c2 = ComplexNumber(2, 3)
         prod = c1 * c2
         self.assertEqual(prod, ComplexNumber(-4, 7))
-        
     def test_divide(self):
         c1 = ComplexNumber(1, 2)
         c2 = ComplexNumber(2, 3)
         div = c1 / c2
-        self.assertEqual(div, ComplexNumber(8/13, 1/13))
-        
+        self.assertEqual(div, ComplexNumber(8 / 13, 1 / 13))
+
     def test_conjugate(self):
         c1 = ComplexNumber(1, 2)
         conjugate = c1.conjugate()
         target = ComplexNumber(1, -2)
         self.assertEqual(conjugate, target)
-
-
 
 
 if __name__ == '__main__':
